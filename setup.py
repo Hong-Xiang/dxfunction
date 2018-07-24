1f4df63c06b6b728e4e104578c9330f926ac9927
HongXiang <hx.hongxiang@gmail.com>
2018-07-24 11:00:49 +0800

    for upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='dxl-function',
-    version='0.1.0',
+    version='0.1.1',
     description='Functional library.',
     url='https://github.com/Hong-Xiang/dxfunction',
     author='Hong Xiang',
